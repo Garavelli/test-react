6b29e3c4219b01105cc0fb1b30d497998c433ec8
Garavelli <maycongaravelli@hotmail.com>
2021-06-26 20:23:39 -0300

    setting functions and effects

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/app.css';
 
-function App() {
+export default function App(): React.ReactElement {
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    console.log('scrolling left: ', scrollLeft)
+  }, [scrollLeft])
+
+  useEffect(() => {
+    console.log('scrolling top: ', scrollTop);
+  }, [scrollTop])
+
+  function handleScrolling(event: React.SyntheticEvent) {
+    const currentScrollLeft = event.currentTarget.scrollLeft;
+    const currentScrollTop = event.currentTarget.scrollTop;
+    if (scrollLeft !== currentScrollLeft) {
+      setScrollLeft(currentScrollLeft);
+    }
+
+    if (scrollTop !== currentScrollTop) {
+      setScrollTop(currentScrollTop);
+    }
+  }
   return (
-    <div className="container">
+    <div className="container" onScroll={handleScrolling}>
       <div id="left" className="scrollBlocks"></div>
       <div id="right" className="scrollBlocks"></div>      
     </div>
   );
-}
-
-export default App;
+};
